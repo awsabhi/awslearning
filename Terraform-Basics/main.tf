de013d8724e92f2e437d09ae921012a52f4faa61
awsabhi <abhishekmishra0791@gmail.com>
2023-08-29 16:52:05 +0530

    1st commit for Terraform

---
+++ Terraform-Basics/main.tf
@@ -0,0 +1,6 @@
+# Configure the AWS Provider
+provider "aws" {
+    access_key = "AKIAQ245UFDBPYQB7KM4"
+    secret_key = "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"
+    region     = "ap-south-1"
+}

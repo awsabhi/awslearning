ddde54d0ff7712c5c63e46e3bf067318a420b8ba
awsabhi <abhishekmishra0791@gmail.com>
2023-08-29 19:57:25 +0530

    Varibales in Terraform

--- Terraform-Basics/createinstance.tf
+++ Terraform-Basics/createinstance.tf
@@ -1,10 +1,5 @@
 resource "aws_instance" "Myfirstinstance" {
-  count = 2
   ami = "ami-0f2e255ec956ade7f"
   instance_type = "t2.micro" 
-  tags = {
-     Name = "demo-instance-${count.index}"
-
-  }
    
 }
--- Terraform-Basics/main.tf
+++
@@ -1,6 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-    access_key = "AKIAQ245UFDBPYQB7KM4"
-    secret_key = "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"
-    region     = "ap-south-1"
-}
